67f98878147a3d5ff1f45dbe9344dea0f895cbcc
Imre Gretsi <imre.gretsi@gmail.com>
2021-09-09 22:44:06 +0300

    Add checkout table depiction in checkout details view
    Also add spinner and error message
    depiction like in book details view.

--- frontend/src/app/components/checkout-detail/checkout-detail.component.ts
+++ frontend/src/app/components/checkout-detail/checkout-detail.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map, switchMap } from 'rxjs/operators';
+import { Checkout } from 'src/app/models/checkout';
+import { CheckoutService } from 'src/app/services/checkout.service';
 
 @Component({
   selector: 'app-checkout-detail',
   templateUrl: './checkout-detail.component.html',
-  styleUrls: ['./checkout-detail.component.scss']
+  styleUrls: ['./checkout-detail.component.scss'],
 })
 export class CheckoutDetailComponent implements OnInit {
+  checkout$: Observable<Checkout>;
+  error: Error;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private checkoutService: CheckoutService
+  ) {}
 
   ngOnInit(): void {
+    this.checkout$ = this.route.params.pipe(map((params) => params.id)).pipe(
+      switchMap((id) =>
+        this.checkoutService.getCheckout(id).pipe(
+          catchError((error) => {
+            this.error = error;
+            return throwError(error);
+          })
+        )
+      )
+    );
   }
-
 }
