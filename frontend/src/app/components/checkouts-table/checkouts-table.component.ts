8b442219a58f584ab2515d4e86c15cdfecba525c
Imre Gretsi <imre.gretsi@gmail.com>
2021-09-13 19:22:27 +0300

    Unsubscribe from observables on unmount of checkouts table component

--- frontend/src/app/components/checkouts-table/checkouts-table.component.ts
+++ frontend/src/app/components/checkouts-table/checkouts-table.component.ts
@@ -1,8 +1,8 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { Router } from '@angular/router';
-import { merge, of } from 'rxjs';
+import { merge, of, Subscription } from 'rxjs';
 import { catchError, map, startWith, switchMap } from 'rxjs/operators';
 import { PageRequest } from 'src/app/models/page';
 import { CheckoutService } from 'src/app/services/checkout.service';
@@ -19,7 +19,7 @@ import { CheckoutsDataSource } from './checkouts-data-source';
   templateUrl: './checkouts-table.component.html',
   styleUrls: ['./checkouts-table.component.scss'],
 })
-export class CheckoutsTableComponent implements AfterViewInit {
+export class CheckoutsTableComponent implements AfterViewInit, OnDestroy {
   dataSource: CheckoutsDataSource = new CheckoutsDataSource([]);
   displayedColumns: string[] = [
     'borrowedBookTitle',
@@ -31,6 +31,7 @@ export class CheckoutsTableComponent implements AfterViewInit {
   ];
   checkoutsCount: number = 0;
   isLoadingCheckouts = true;
+  subscriptions: Subscription[] = [];
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
@@ -42,41 +43,49 @@ export class CheckoutsTableComponent implements AfterViewInit {
 
   ngAfterViewInit(): void {
     // Rollback to first page on sorting change
-    this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0));
+    this.subscriptions.push(
+      this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0))
+    );
 
     // Get new page from checkoutService on sorting change or on navigation to next/previous page
-    merge(this.paginator.page, this.sort.sortChange)
-      .pipe(
-        startWith({}),
-        switchMap(() => {
-          let sort: PageRequest['sort'];
-          // multiple checkout fields sorting is used
-          // because 'borrower' is the column composed from two fields of checkout (borrowerFirstName, borrowerLastName)
-          if (this.sort.active === 'borrower') {
-            sort = [
-              { column: 'borrowerFirstName', direction: this.sort.direction },
-              { column: 'borrowerLastName', direction: this.sort.direction },
-            ];
-          } else {
-            sort = [
-              { column: this.sort.active, direction: this.sort.direction },
-            ];
-          }
-          this.isLoadingCheckouts = true;
-          return this.checkoutService
-            .getCheckouts({
-              pageIndex: this.paginator.pageIndex,
-              sort,
-            })
-            .pipe(catchError(() => of({ totalElements: 0, content: [] })));
-        }),
-        map((page) => {
-          this.isLoadingCheckouts = false;
-          this.checkoutsCount = page.totalElements;
-          return page.content;
-        })
-      )
-      .subscribe((checkouts) => this.dataSource.setData(checkouts));
+    this.subscriptions.push(
+      merge(this.paginator.page, this.sort.sortChange)
+        .pipe(
+          startWith({}),
+          switchMap(() => {
+            let sort: PageRequest['sort'];
+            // multiple checkout fields sorting is used
+            // because 'borrower' is the column composed from two fields of checkout (borrowerFirstName, borrowerLastName)
+            if (this.sort.active === 'borrower') {
+              sort = [
+                { column: 'borrowerFirstName', direction: this.sort.direction },
+                { column: 'borrowerLastName', direction: this.sort.direction },
+              ];
+            } else {
+              sort = [
+                { column: this.sort.active, direction: this.sort.direction },
+              ];
+            }
+            this.isLoadingCheckouts = true;
+            return this.checkoutService
+              .getCheckouts({
+                pageIndex: this.paginator.pageIndex,
+                sort,
+              })
+              .pipe(catchError(() => of({ totalElements: 0, content: [] })));
+          }),
+          map((page) => {
+            this.isLoadingCheckouts = false;
+            this.checkoutsCount = page.totalElements;
+            return page.content;
+          })
+        )
+        .subscribe((checkouts) => this.dataSource.setData(checkouts))
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
   }
 
   // Used on checkout row click for navigation to checkout details view
