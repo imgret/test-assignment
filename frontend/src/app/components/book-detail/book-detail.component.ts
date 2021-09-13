7a95661bbfe250ed3e07c3dc2fc3f711c36eeab8
Imre Gretsi <imre.gretsi@gmail.com>
2021-09-13 20:32:06 +0300

    Handle submission of book checking out in book details view
    - Set initial values to book checkout
      form on opening of this form.
    - Add handler to submit event of book
      checkout form.

--- frontend/src/app/components/book-detail/book-detail.component.ts
+++ frontend/src/app/components/book-detail/book-detail.component.ts
@@ -1,18 +1,20 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { BookService } from '../../services/book.service';
 import { Book } from '../../models/book';
-import { Subscription, throwError } from 'rxjs';
+import { OperatorFunction, Subscription, throwError } from 'rxjs';
 import { ActivatedRoute, Router } from '@angular/router';
 import { catchError, map, switchMap } from 'rxjs/operators';
 import { MatDialog } from '@angular/material/dialog';
 import { ConfirmationDialogComponent } from '../confirmation-dialog/confirmation-dialog.component';
+import { Checkout } from 'src/app/models/checkout';
+import { CheckoutService } from 'src/app/services/checkout.service';
 
 @Component({
   selector: 'app-book-detail',
   templateUrl: './book-detail.component.html',
   styleUrls: ['./book-detail.component.scss'],
 })
-// TODO Add checkout creation form into checkouts view or into book details view (prefer book details)
+// TODO Add dialog for checkout confirmation
 export class BookDetailComponent implements OnInit, OnDestroy {
   book: Book;
   error: Error;
@@ -20,10 +22,12 @@ export class BookDetailComponent implements OnInit, OnDestroy {
   isProcessingRequest: boolean;
   showEditBook: boolean;
   showCheckOutBook: boolean;
+  checkout: Checkout;
 
   constructor(
     private route: ActivatedRoute,
     private bookService: BookService,
+    private checkoutService: CheckoutService,
     private router: Router,
     public dialog: MatDialog
   ) {}
@@ -116,8 +120,55 @@ export class BookDetailComponent implements OnInit, OnDestroy {
 
   // Toggle depiction of book checkout form.
   toggleCheckOutBook() {
-    this.showCheckOutBook = !this.showCheckOutBook;
-    // Close book editing form on opening of checkout creation form
-    this.showEditBook = false;
+    if (this.showCheckOutBook) {
+      this.showCheckOutBook = false;
+      this.checkout = null;
+    } else {
+      this.showCheckOutBook = true;
+      this.checkout = {
+        id: '',
+        borrowerFirstName: '',
+        borrowerLastName: '',
+        borrowedBook: this.book,
+        checkedOutDate: new Date().toISOString().split('T')[0],
+        dueDate: '',
+        returnedDate: '',
+      };
+      // Close book editing form on opening of checkout creation form
+      this.showEditBook = false;
+    }
+  }
+
+  // Rxjs pipeable operator, which sets status of current book to 'BORROWED' and
+  // returns observable which emits updated book object.
+  setBookStateBorrowed(): OperatorFunction<unknown, Book> {
+    const updatedBook: Book = { ...this.book, status: 'BORROWED' };
+    return (input$) =>
+      input$.pipe(
+        switchMap(() => this.bookService.saveBook(updatedBook)),
+        map(() => updatedBook)
+      );
+  }
+
+  // Used on submit event from book checkout form.
+  // Calls saveCheckout of checkout service to create new checkout entry in a database and
+  // sets book state to BORROWED in the database and updates book state in view.
+  checkOutBook(checkout: Checkout) {
+    this.error = null;
+    this.isProcessingRequest = true;
+    this.checkoutService
+      .saveCheckout(checkout)
+      .pipe(
+        this.setBookStateBorrowed(),
+        catchError((error) => {
+          this.error = error;
+          this.isProcessingRequest = false;
+          return throwError(error);
+        })
+      )
+      .subscribe((book) => {
+        this.book = book;
+        this.isProcessingRequest = false;
+      });
   }
 }
