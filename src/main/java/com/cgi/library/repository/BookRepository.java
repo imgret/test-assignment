fb2baf760188b58399541f6893877f82689fc2f4
Imre Gretsi <imre.gretsi@gmail.com>
2021-09-15 21:53:55 +0300

    Add comment to custom free text book query

--- src/main/java/com/cgi/library/repository/BookRepository.java
+++ src/main/java/com/cgi/library/repository/BookRepository.java
@@ -18,6 +18,11 @@ public interface BookRepository extends JpaRepository<Book, UUID> {
     // - http://www.h2database.com/html/functions.html#concat_ws
     // - https://www.baeldung.com/spring-jpa-like-queries
     // - https://www.baeldung.com/spring-data-jpa-query
+    // This part was pretty challenging, because before writing this simple sql query
+    // I tried to use hibernate search like in https://www.baeldung.com/hibernate-search.
+    // I spent a lot of time setting hibernate search up and at the end it even managed to run,
+    // however it was not properly working. Every query returned empty list.
+    // So I decided to roll back and try another approach.
     @Query("SELECT b from Book b where CONCAT_WS(' ', b.id, b.title, b.author, b.genre, b.year, b.added, b.checkOutCount, b.status, b.dueDate, b.comment) like %:searchTerm%")
     Page<Book> searchFreeText(@Param("searchTerm") String searchTerm, Pageable pageable);
 }
